e39fd0a9c9c362fd7c105edade1178d3a5c99790
angtaichishang <angtaichishang@gmail.com>
2024-11-19 13:25:46 +0900

    section4

--- section4-2/sketch.js
+++ section4-2/sketch.js
@@ -4,30 +4,31 @@
 
 let balls;
 
-function setup(){
+function setup() {
   createCanvas(windowWidth, windowHeight);
   balls = [];
 }
 
-function draw(){
+function draw() {
   background(160, 192, 255);
-  for(let i = 0; i < balls.length; i++){
+  for (let i = 0; i < balls.length; i++) {
     let b = balls[i];
     ellipse(b.x, b.y, b.size);
     b.x += b.vx;
     b.y += b.vy;
+    b.size = random(10, 100);
   }
-}
-
-function mouseDragged(){
-  const dx = mouseX - pmouseX;
-  const dy = mouseY - pmouseY;
-  if(mag(dx, dy) > 5){
-    const b = { x: mouseX, y: mouseY, size: 20, vx: dx, vy: dy };
+  if (frameCount % 30 === 0) {
+    const b = {
+      x: random(width),
+      y: random(height),
+      vx: random(-3, 3),
+      vy: random(-3, 3),
+    };
     balls.push(b);
   }
 }
 
-function windowResized(){
+function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
